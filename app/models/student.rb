7313ad0d5825f3dfdb4ed1f58edf14bcce795450
Victor <vx_zz@hotmail.com>
2022-08-26 13:09:07 -0400

    fixed Student, dependent destroy

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,7 +7,7 @@ class Student < ApplicationRecord
     has_many :course_students, dependent: :destroy
     has_many :courses, through: :course_students
     has_many :discussion_posts, dependent: :destroy
-    has_many :submissions
+    has_many :submissions, dependent: :destroy
     has_secure_password
 
 end
